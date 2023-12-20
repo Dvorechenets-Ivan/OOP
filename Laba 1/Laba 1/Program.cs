aacd4bd974bd04fb45274c4770e5493f7c4949e1
Dvorechenets-Ivan <135117798+Dvorechenets-Ivan@users.noreply.github.com>
2023-12-20 23:53:25 +0200

    Add files via upload

---
+++ Laba 1/Laba 1/Program.cs
@@ -0,0 +1,140 @@
+﻿﻿using System;
+using System.Xml.Serialization;
+
+class Matrix
+{
+    private int[,] data;
+
+    public int Rows { get; private set; }
+    public int Columns { get; private set; }
+
+    // Конструктор для створення матриці заданого розміру
+    public Matrix(int rows, int columns)
+    {
+        Rows = rows;
+        Columns = columns;
+        data = new int[rows, columns];
+    }
+
+    // Індексатор для доступу до елементів матриці
+    public int this[int row, int column]
+    {
+        get { return data[row, column]; }
+        set { data[row, column] = value; }
+    }
+
+    // Метод для введення матриці з консолі
+    public void InputMatrix()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                Console.Write($"Введіть елемент [{i},{j}]: ");
+                data[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+    // Метод для виведення матриці на консоль
+    public void OutputMatrix()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                Console.Write(data[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // Метод для знаходження максимального елемента матриці
+    public int FindMax()
+    {
+        int max = data[0, 0];
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                if (data[i, j] > max)
+                {
+                    max = data[i, j];
+                }
+            }
+        }
+        return max;
+    }
+
+    // Метод для знаходження мінімального елемента матриці
+    public int FindMin()
+    {
+        int min = data[0, 0];
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                if (data[i, j] < min)
+                {
+                    min = data[i, j];
+                }
+            }
+        }
+        return min;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо об'єкт матриці розміром 3x3
+
+        Console.WriteLine("Введіть кількість рядків матриці:");
+        int row = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введіть кількість стовпців матриці:");
+        int col = int.Parse(Console.ReadLine());
+        Matrix matrix = new Matrix(row, col);
+
+        Console.WriteLine("Бажаєте ввести матрицю вручну (введіть '1') чи згенерувати рандомну (введіть '2')?");
+        int choice = int.Parse(Console.ReadLine());
+        if (choice == 1)
+        {
+            Console.WriteLine("Введіть матрицю:");
+            matrix.InputMatrix();
+        }
+        else if (choice == 2)
+        {
+            Random random = new Random();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = random.Next(-101, 101);
+                }
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Невірний вибір. Будь ласка, введіть '1' для введення вручну або '2' для генерації рандомно.");
+            return;
+        }
+
+
+
+        // Вводимо матрицю з консолі
+
+
+        // Виводимо матрицю на консоль
+        Console.WriteLine("Матриця:");
+        matrix.OutputMatrix();
+
+        // Знаходимо та виводимо максимальний та мінімальний елементи
+        int max = matrix.FindMax();
+        int min = matrix.FindMin();
+        Console.WriteLine($"Максимальний елемент: {max}");
+        Console.WriteLine($"Мінімальний елемент: {min}");
+    }
+}
---
+++ Laba 1/Завдання 2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+public class TSquare
+{
+    private double sideLength;
+
+    // Конструктори і методи класу TSquare
+
+    public override string ToString()
+    {
+        return $"Квадрат зі стороною {sideLength}";
+    }
+
+    public double CalculateArea()
+    {
+        return sideLength * sideLength;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 4 * sideLength;
+    }
+
+    public bool CompareTo(TSquare otherSquare)
+    {
+        return this.sideLength == otherSquare.sideLength;
+    }
+
+    public static TSquare operator +(TSquare square1, TSquare square2)
+    {
+        double newSideLength = square1.sideLength + square2.sideLength;
+        return new TSquare(newSideLength);
+    }
+
+    public static TSquare operator -(TSquare square1, TSquare square2)
+    {
+        double newSideLength = Math.Max(0, square1.sideLength - square2.sideLength);
+        return new TSquare(newSideLength);
+    }
+
+    public static TSquare operator *(TSquare square, double multiplier)
+    {
+        double newSideLength = square.sideLength * multiplier;
+        return new TSquare(newSideLength);
+    }
+
+    public TSquare()
+    {
+        sideLength = 0.0;
+    }
+
+    public TSquare(double side)
+    {
+        sideLength = side;
+    }
+
+    public TSquare(TSquare otherSquare)
+    {
+        sideLength = otherSquare.sideLength;
+    }
+
+    public void InputData()
+    {
+        Console.Write("Введіть довжину сторони квадрата: ");
+        sideLength = double.Parse(Console.ReadLine());
+    }
+
+    public void DisplayData()
+    {
+        Console.WriteLine(ToString());
+        Console.WriteLine($"Площа: {CalculateArea()}");
+        Console.WriteLine($"Периметр: {CalculatePerimeter()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Приклад використання класу TSquare
+        Console.WriteLine("Створення першого квадрата:");
+        TSquare square1 = new TSquare();
+        square1.InputData();
+        square1.DisplayData();
+
+        Console.WriteLine("\nСтворення другого квадрата:");
+        TSquare square2 = new TSquare();
+        square1.InputData();
+        square1.DisplayData();
+
+        Console.WriteLine("\nПеревірка на рівність двох квадратів:");
+        bool areEqual = square1.CompareTo(square2);
+        Console.WriteLine($"Квадрат 1 рівний квадрату 2: {areEqual}");
+
+        Console.WriteLine("\nДодавання двох квадратів:");
+        TSquare sumSquare = square1 + square2;
+        sumSquare.DisplayData();
+
+        Console.WriteLine("\nВіднімання другого квадрата від першого:");
+        TSquare subSquare = square1 - square2;
+        subSquare.DisplayData();
+
+        Console.WriteLine("\nМноження першого квадрата на 2:");
+        TSquare multSquare = square1 * 2;
+        multSquare.DisplayData();
+    }
+}
