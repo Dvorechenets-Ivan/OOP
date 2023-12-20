1833a0f47c8b97de0bab7d9311ffec40f51d4c85
Dvorechenets-Ivan <135117798+Dvorechenets-Ivan@users.noreply.github.com>
2023-12-21 01:46:49 +0200

    Add files via upload

---
+++ Laba4/lab_4/MyComplex.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    public class MyComplex : IMyNumber<MyComplex>
+    {
+        private double re;
+        private double mi;
+
+        public MyComplex(double re, double mi)
+        {
+            this.re = re;
+            this.mi = mi;
+        }
+
+        public MyComplex Add(MyComplex that)
+        {
+            return new MyComplex(this.re + that.re, this.mi + that.mi);
+
+        }
+
+        public MyComplex Subtract(MyComplex that)
+        {
+            return new MyComplex(this.re - that.re, this.mi - that.mi);
+        }
+
+        public MyComplex Multiply(MyComplex that)
+        {
+            return new MyComplex(((this.re * that.re) - (this.mi * that.mi)), (this.re * that.mi) + (this.mi * that.re));
+        }
+
+        public MyComplex Divide(MyComplex that)
+        {
+            if (that.re == 0)
+            {
+
+                throw new DivideByZeroException();
+
+            }
+            return new MyComplex((((this.re * that.re) + (this.mi * that.mi)) /
+                               (Math.Pow(that.re, 2) + Math.Pow(that.mi, 2))),
+
+                               (((this.mi * that.re) - (this.re * that.mi)) /
+                               (Math.Pow(that.re, 2) + Math.Pow(that.mi, 2))));
+
+
+
+        }
+        public override string ToString()
+        {
+            return $"{re} + {mi}i";
+        }
+    }
+}
---
+++ Laba4/lab_4/MyFrac.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Lab_4
+{
+    public class MyFrac : IMyNumber<MyFrac>, IComparable<MyFrac>
+    {
+        private BigInteger nom;
+        private BigInteger denom;
+
+        public MyFrac(int nom, int denom)
+        {
+            this.nom = nom;
+            this.denom = denom;
+            Simplify();
+        }
+
+        public MyFrac(BigInteger nom, BigInteger denom)
+        {
+            this.nom = nom;
+            this.denom = denom;
+            Simplify();
+        }
+
+        private static BigInteger GCD(BigInteger a, BigInteger b)
+        {
+            return BigInteger.GreatestCommonDivisor(a, b);
+        }
+
+        private void Simplify()
+        {
+            BigInteger gcd = GCD(nom, denom);
+            nom /= gcd;
+            denom /= gcd;
+        }
+
+        public MyFrac Add(MyFrac that)
+        {
+            MyFrac result = new MyFrac(BigInteger.Add(BigInteger.Multiply(nom, that.denom),
+                                       BigInteger.Multiply(denom, that.nom)),
+                                       BigInteger.Multiply(denom, that.denom));
+            result.Simplify();
+            return result;
+        }
+
+        public MyFrac Subtract(MyFrac that)
+        {
+            MyFrac result = new MyFrac(BigInteger.Subtract(BigInteger.Multiply(nom, that.denom),
+                                       BigInteger.Multiply(denom, that.nom)),
+                                       BigInteger.Multiply(denom, that.denom));
+            result.Simplify();
+            return result;
+        }
+
+        public MyFrac Multiply(MyFrac that)
+        {
+            MyFrac result = new MyFrac(BigInteger.Multiply(nom, that.nom),
+                                       BigInteger.Multiply(denom, that.denom));
+            result.Simplify();
+            return result;
+        }
+
+        public MyFrac Divide(MyFrac that)
+        {
+            if (denom == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            MyFrac result = new MyFrac(BigInteger.Multiply(nom, that.denom),
+                                       BigInteger.Multiply(denom, that.nom));
+            result.Simplify();
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"{nom} / {denom}";
+        }
+
+
+        public int CompareTo(MyFrac that)
+        {
+
+            MyFrac thisNormalized = new MyFrac(nom, denom);
+            MyFrac thatNormalized = new MyFrac(that.nom, that.denom);
+
+            thisNormalized.Simplify();
+            thatNormalized.Simplify();
+
+            return (int)(thisNormalized.nom * thatNormalized.denom - thisNormalized.denom * thatNormalized.nom);
+        }
+
+    }
+
+}
